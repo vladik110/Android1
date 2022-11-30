94e2d6e7aaeb06be92c26066baf11a468733d1ee
vladik110 <119513745+vladik110@users.noreply.github.com>
2022-11-30 20:48:49 +0600

    Add files via upload

---
+++ TSN_DEMO_01_Sum/src/main/java/kz/talipovsn/sum/MainActivity.java
@@ -0,0 +1,61 @@
+package kz.talipovsn.sum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText_a; // Переменная для доступа к компоненту со значением "a"
+    EditText editText_b; // Переменная для доступа к компоненту со значением "b"
+    EditText editText_x; // Переменная для доступа к компоненту со значением "x"
+    TextView textView_sum; // Переменная для доступа к компоненту со значением результата
+    Button buttonSum; // Переменная для доступа к компоненту кнопки
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Доступ к компонентам окна
+        editText_a = findViewById(R.id.editText_a);
+        editText_b = findViewById(R.id.editText_b);
+        editText_x = findViewById(R.id.editText_x);
+        textView_sum = findViewById(R.id.textView_sum);
+        buttonSum = findViewById(R.id.buttonSum);
+    }
+
+    // МЕТОД КНОПКИ РАСЧЕТА
+    public void onClick(View v) {
+        // Объявление локальных переменных
+        double a, b, c, x;
+
+        try { // НАЧАЛО ЗАЩИЩЕННОГО БЛОКА
+
+            // Чтение данных из компонент
+            a = Double.parseDouble(editText_a.getText().toString().trim());
+            b = Double.parseDouble(editText_b.getText().toString().trim());
+            x = Double.parseDouble(editText_x.getText().toString().trim());
+
+            // Основной алгоритм
+            if (x >= 4)
+            c = (x + (4 * a)) / (a * a) * (b * b);
+            else
+            c = (x * x * x) - (a*b);
+
+            // Вывод полученного значения в компонент
+            textView_sum.setText(String.valueOf(c));
+
+        } catch (Exception e) { // ЧТО ДЕЛАТЬ ЕСЛИ ВОЗНИКНЕТ ОШИБКА В БЛОКЕ МЕЖДУ "TRY" И "CATCH":
+
+            // Вывод сообщения об ошибке
+            textView_sum.setText("Неверные входные данные для расчета!");
+
+        }  // КОНЕЦ ЗАЩИЩЕННОГО БЛОКА
+
+    }
+
+}
